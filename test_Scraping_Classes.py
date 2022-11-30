0b137bc81155a0ae07850b7c55cfd4f0294d3556
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2022-11-30 15:40:26 +0000

    Adds functionality to test_scrape_from_crawler that checks that the keys of the dictionary the data is being stored in are all correct

--- test_Scraping_Classes.py
+++ test_Scraping_Classes.py
@@ -20,26 +20,32 @@ class Scraper_ObjectTestCase(unittest.TestCase):
         self.tester = SC.Scraper_Object('energy supplier', 'https://www.trustpilot.com/')
         
 
-    #def test_create_crawler(self):
+    def test_create_crawler(self):
         #Calls the create_crawler method on self.tester with a length argument of 1
-        #self.tester.create_crawler(2)
+        self.tester.create_crawler(2)
     
         #Checks that the lenght of the crawler is equal to the number input into create_crawler
-        #length = len(self.tester.crawler)
-        #self.assertEqual(2, length)
+        length = len(self.tester.crawler)
+        self.assertEqual(2, length)
         #Checks whether the crawler is a list
-        #self.assertIsInstance(self.tester.crawler, list, "Crawler is not a list object")
+        self.assertIsInstance(self.tester.crawler, list, "Crawler is not a list object")
         #Checks whether the items inside the crawler are strings
-        #self.assertIsInstance(self.tester.crawler[0], str, "Url in crawler is not a string object")
+        self.assertIsInstance(self.tester.crawler[0], str, "Url in crawler is not a string object")
 
     def test_scrape_from_cralwer(self):
         self.tester.crawler = ['https://www.trustpilot.com/review/integrityenergy.com']
         self.tester.scrape_from_crawler()
         data = self.tester.scraped_data
+        print(data)
         #Checks that data is a list
         self.assertIsInstance(data, list, "The scraped data is not stored in a list")
         #Checks that the list elements (representing data for one specific business) are dictionaries 
         self.assertIsInstance(data[0], dict, "The elements in data are not dictionary objects")
+        #The values of the dictionary are liable to change over time; however we can check that all of the keys are correct
+        correct_keys = ['Name', 'ID', 'Timestamp', 'Href', 'Number of Reviews', 'Rating', 'Email']
+        actual_keys = list(data[0].keys())
+        self.assertTrue(correct_keys == actual_keys, "The keys of the dictionary in self.scraped_data are not correct.")
+
         
 if __name__ == "__main__":
     unittest.main()
